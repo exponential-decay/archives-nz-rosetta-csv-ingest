ffeec5a21855cbdbee65073552c796f9dadab8eb
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2014-08-29 20:03:23 +1200

    [feature] Creating CSV from config file.

--- rosettacsvsectionsclass.py
+++ rosettacsvsectionsclass.py
@@ -1,15 +1,29 @@
+import ConfigParser
+
 class RosettaCSVSections:
    
    sections = []
    
    def __init__(self):
-      
-      IE = {"IE": ["Title", "Access", "Archway Identifier for publishing", "Archway Identifier Type",	"Archway Identifier Value", "Series", "Agency", "Entity Type", "Submission Reason"]}
+ 
+      self.config = ConfigParser.RawConfigParser()
+      self.config.read('rosetta-csv-mapping.cfg')   
 
-      REPRESENTATION = {"REPRESENTATION": ["Revision Number", "Usage Type", "Preservation Type"]}
-
-      FILE = {"FILE": ["File Location", "File Original Path", "File Name", "File fixity type", "File fixity value"]}
-
-      self.sections.append(IE)
-      self.sections.append(REPRESENTATION)
-      self.sections.append(FILE)
+      # Configure via CFG to avoid users having to edit code
+      if self.config.has_option('rosetta csv fields', 'CSVSECTIONS'):     
+         sections = self.config.get('rosetta csv fields', 'CSVSECTIONS').split(',')
+       
+      self.sect = []
+      for section in sections:     
+         if self.config.has_option('rosetta csv fields', section):
+            sectiondict = {}
+            fieldlist = self.config.get('rosetta csv fields', section)
+            sectiondict[section] = fieldlist.split(',')
+            initsuccess = True
+            
+            self.sections.append(sectiondict)
+         else:
+            sys.stdout.write("Error reading fields from config file, exiting...")
+            sys.exit(1) # poor-form exiting from a child class? 
+            break
+            
