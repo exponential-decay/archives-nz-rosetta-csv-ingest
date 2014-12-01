d01b08acda5730dd021716663808ff706d959d53
ross-spencer <all.along.the.watchtower2001@gmail.com>
2014-12-01 21:55:11 +1300

    [tests] Compare filenames as titles tests.

--- RosettaCSVGeneratorTests.py
+++ RosettaCSVGeneratorTests.py
@@ -50,6 +50,33 @@ class RosettaCSVGeneratorTests(TestCase):
       self.assertEqual(self.rosettaCSVgen.normalize_spaces(case_nospace), result_nospace)
       self.assertEqual(self.rosettaCSVgen.normalize_spaces(case_onespace), result_onespace)
 
+   def test_compare_filenames_as_titles(self):
+      self.setup()
+      
+      # Standard true comparison, expected result
+      case_comparison_no_dot = {u'STATUS': u'Done', u'MIME_TYPE': u'application/msword', u'NAME': u'FILENAME', u'PUID': u'fmt/1'}   #truncated dict
+      result_comparison_no_dot = "FILENAME"
+
+      # Standard true comparison, expected result
+      case_comparison_dot = {u'STATUS': u'Done', u'MIME_TYPE': u'application/msword', u'NAME': u'NEWFILENAME.DOC', u'PUID': u'fmt/1'}
+      result_comparison_dot = "NEWFILENAME"
+      
+      # False result: Checks that stipping of extension happens
+      case_false_result = {u'STATUS': u'Done', u'MIME_TYPE': u'application/msword', u'NAME': u'FALSENAME.DOC', u'PUID': u'fmt/1'}
+      result_false_result = "FALSENAME.DOC"
+      
+      # Multiple dots should only see removal of one
+      case_multiple_dots = {u'STATUS': u'Done', u'MIME_TYPE': u'application/msword', u'NAME': u'MANY.EXT.EXT.DOC', u'PUID': u'fmt/1'}
+      result_multiple_dots = "MANY.EXT.EXT"
+      
+      #TRUE tests
+      self.assertTrue(self.rosettaCSVgen.compare_filenames_as_titles(case_comparison_no_dot, result_comparison_no_dot))
+      self.assertTrue(self.rosettaCSVgen.compare_filenames_as_titles(case_comparison_dot, result_comparison_dot))
+      self.assertTrue(self.rosettaCSVgen.compare_filenames_as_titles(case_multiple_dots, result_multiple_dots))
+
+      #FALSE tests
+      self.assertFalse(self.rosettaCSVgen.compare_filenames_as_titles(case_false_result, result_false_result))
+
 def main():
 	suite = TestLoader().loadTestsFromTestCase(RosettaCSVGeneratorTests)
 	TextTestRunner().run(suite)
