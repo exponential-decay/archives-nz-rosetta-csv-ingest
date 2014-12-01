f44d60ce60d958c75b2dd2ee338045a2981f11af
ross-spencer <all.along.the.watchtower2001@gmail.com>
2014-12-01 20:07:02 +1300

    [tests] Case variables and result variables.

--- RosettaCSVGeneratorTests.py
+++ RosettaCSVGeneratorTests.py
@@ -8,6 +8,28 @@ class RosettaCSVGeneratorTests(TestCase):
 
    def test_normalize_spaces(self):
       self.setup()
+      
+      case_blank_two_spaces = "  "
+      result_blank_two_spaces = " "
+      
+      case_words_two_spaces = "one  two"
+      result_words_two_spaces = "one two"
+      
+      case_words_three_spaces = "one   two"
+      result_words_three_spaces = "one two"
+      
+      case_multiple_words_spaces = "one  two  three"
+      result_multiple_words_spaces = "one two three"
+      
+      case_filename = "001 SPREADSHEET  FILE.xls" 
+      result_filename = "001 SPREADSHEET FILE.xls"
+      
+      case_nospace = "word"
+      result_nospace = "word"
+      
+      case_onespace = "word space"
+      result_onespace = "word space"
+      
       self.assertEqual(" ", self.rosettaCSVgen.normalize_spaces("  "))
 
 def main():
