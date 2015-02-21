e808eb2c989abce6f71200eb3eaff4e35bab8ead
ross-spencer <all.along.the.watchtower2001@gmail.com>
2015-02-21 22:00:16 +1300

    [feature] Better error handling.

--- ProvenanceCSVHandlerClass.py
+++ ProvenanceCSVHandlerClass.py
@@ -1,12 +1,26 @@
 import unicodecsv
+import sys
 
 from droidcsvhandlerclass import *
 
 class provenanceCSVHandler:
 
+   #TODO: Better error handlin? Format error handling? Validation?
+   provheaders = ['RECORDNUMBER','NOTEDATE','NOTETEXT']
+
    def readProvenanceCSV(self, provcsvname):
       csvhandler = genericCSVHandler()
       exportlist = csvhandler.csvaslist(provcsvname)
+      #counter a blank sheet
+      if len(exportlist) < 1:
+         exportlist = None
+      if exportlist is not None:
+         for h in self.provheaders:
+            if h in exportlist[0]:
+               sys.stderr.write(str(h) + '\n')
+            else:
+               exportlist = None
+               break
       return exportlist
       
       
