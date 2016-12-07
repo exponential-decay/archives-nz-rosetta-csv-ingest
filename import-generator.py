ca72fc081c2c35f653d08577fdb23dfbfbaf7d91
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-12-07 14:17:32 +1300

    [refactor] making this more useable.

--- import-generator.py
+++ import-generator.py
@@ -39,7 +39,7 @@ def main():
    #NOTE: class on its own might be used to create a blank import csv with just static options
    parser.add_argument('--csv', help='Single DROID CSV to read.', default=False, required=False)
    parser.add_argument('--imp', help='Archway import schema to use.', default=False, required=False)
-   parser.add_argument('--exp', help='Archway export sheet to map to Rosetta ingest CSV', default=False, required=False)
+   parser.add_argument('--exp', help='Archway list control sheet to map to Rosetta ingest CSV', default=False, required=False)
    parser.add_argument('--ros', help='Rosetta CSV validation schema', default=False, required=False)
    parser.add_argument('--cfg', help='Config file for field mapping.', default=False, required=False)
    parser.add_argument('--pro','--prov', help='Flag to enable use of prov.notes file.', default=False, required=False, action="store_true")
@@ -57,12 +57,14 @@ def main():
    
    #TODO: Additional help text to describe two discrete sets of options
    if args.args:
+   
       config = ConfigParser.RawConfigParser()
       config.read(args.args)   
+      
       if config.has_option('arguments', 'title'):
-         sys.stderr.write("INFO: Using " + config.get('arguments', 'title') + " args configuration file." + "\n")
+         sys.stderr.write("INFO: Using " + config.get('arguments', 'title') + " configuration file." + "\n")
       else:
-         sys.stderr.write("Using an arguments configuration file.")
+         sys.stderr.write("Using an arguments configuration file." + "\n\n")
 
       if config.has_option('arguments', 'droidexport'):
          args.csv = config.get('arguments', 'droidexport')
@@ -71,7 +73,12 @@ def main():
          if config.get('arguments', 'ingest').lower() == "true":    #we need a list control for ingest
             args.exp = config.get('arguments', 'listcontrol')
          else:
+            args.imp = True
             args.exp = False
+            
+      if args.imp == True and args.exp == False:
+         if config.has_option('arguments', 'impconfig'):
+            args.cfg = config.get('arguments', 'impconfig')
    
       #special option for creating an import cover sheet
       if config.has_option('arguments', 'coversheet'):
@@ -83,12 +90,16 @@ def main():
          if config.get('arguments', 'xml').lower() == 'true':
             args.xml = True
    
+   #creating an import sheet for Archway...
    if args.csv and args.imp and args.cfg:
       importsheetDROIDmapping(args.csv, args.imp, args.cfg)
+   #creating an ingest sheet for Rosetta...
    elif args.csv and args.exp and args.ros and args.cfg:
       exportsheetRosettamapping(args.csv, args.exp, args.ros, args.cfg, args.pro, args.xml)
+   #creating a cover sheet for Archway...
    elif args.csv and args.cfg:
       createImportOverview(args.csv, args.cfg)
+   #we're not doing anything sensible...
    else:
       parser.print_help()
       sys.exit(1)
